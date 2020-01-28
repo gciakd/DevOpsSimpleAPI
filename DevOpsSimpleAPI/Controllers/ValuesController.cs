8ec150bcd26c32432c76d12c6447b832fa1ebf35
Ashok Deb <adeb@gsu.edu>
2020-01-28 12:33:04 -0500

    Changed value to NameValue

--- DevOpsSimpleAPI/Controllers/ValuesController.cs
+++ DevOpsSimpleAPI/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace DevOpsSimpleAPI.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "NameValue1", "NameValue2" };
         }
 
         // GET api/values/5
